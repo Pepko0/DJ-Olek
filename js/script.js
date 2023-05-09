49d8ed445ea502476073fd2e22d8629783d09d69
Kacper Lewko <k.lewkosga@gmail.com>
2023-05-09 11:11:33 +0200

    added new mobileheader

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-const lala = () => {
-window.addEventListener("resize", () => {
-    const screenWidth = window.innerWidth;
-    console.log(`Aktualna szerokość strony: ${screenWidth}px`);
-  })
-}
+{
+  const toggleMobileHeader = () => {
+    const buttonAddClass = document.querySelector(".js-buttonHeader")
+    const link = document.querySelector(".js-mobileHeader")
 
-  const header = () => {
-    // const screenWidth = window.innerWidth;
-    
-    console.log(`${(lala.screenWidth)}`) ;
-    lala();
+
+    buttonAddClass.addEventListener("click", () => {
+      link.classList.toggle("disable");
+    })
+
+  }
+
+  const hiddenMobileHeader = () => {
+    const mobileLink = document.querySelector(".js-mobileLink")
+    const headerMobile = document.querySelector(".js-mobileHeader")
+
+    mobileLink.addEventListener("click", () => {
+      headerMobile.classList.add("disable");
+    })
+  }
+
+  const init = () => {
+
+    hiddenMobileHeader();
+    toggleMobileHeader(); 
+
+
+  }
+  init();
+
 }
-  header();
